74189ec6abfd7cefe4533d1aa3edfbbb85305b7a
Tero Rantanen <t1rate01@students.oamk.fi>
2023-02-24 18:40:23 +0200

    Attempt

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+var maintext = "Hello World!";
+
+document.getElementById("maintext").innerHTML = maintext;
+
